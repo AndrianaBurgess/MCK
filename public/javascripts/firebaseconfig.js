5ded98400a51896e3e5315507b9e980653b7c39d
Brandon <brandon.cole@bison.howard.edu>
2018-12-05 13:10:31 -0500

    put firebase config in js file

---
+++ public/javascripts/firebaseconfig.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCH4d3-ll83q58Ky0nku9KQfg2zGaNpsug",
+    authDomain: "mck-224606.firebaseapp.com",
+    databaseURL: "https://mck-224606.firebaseio.com",
+    projectId: "mck-224606",
+    storageBucket: "mck-224606.appspot.com",
+    messagingSenderId: "796374360071"
+};
+  firebase.initializeApp(config);
