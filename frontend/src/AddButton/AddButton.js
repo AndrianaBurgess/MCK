9237e30f5952b6b567fc77015d58cf31130bae58
Brandon <brandon.cole@bison.howard.edu>
2018-12-09 20:55:16 -0500

    Added component to upload product to firebase

---
+++ frontend/src/AddButton/AddButton.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+
+class AddButton extends Component {
+
+    constructor(){
+        super();
+        this.firestore = firebase.firestore();
+    }
+
+    uploadProduct = () => {
+        var userEmail = this.props.email;
+        var product = this.props.product;
+
+        this.firestore.collection('users')
+        .doc(userEmail).collection('products')
+        .doc(product['id']).set(product);
+    }
+
+    render() {
+        return (
+            <button onclick={this.uploadProduct}> Upload </button>
+        );
+      }
+}
+
+export default AddButton;
