60e2cb3a51a0f42d9db4ed1eeb0debf4ea1da4ea
Andriana Burgess <andriana.burgess@bison.howard.edu>
2018-12-11 15:20:31 -0500

    Form set up

--- frontend/src/Components/NewProductUI.js
+++ frontend/src/Components/NewProductUI.js
@@ -9,20 +9,23 @@ class NewProductUI extends Component {
         this.firestore = firebase.firestore();
     }
 
+
+
     render() {
         return (
-            <div>
-               Name: <input></input>
-               Brand: <input></input>
-               Rating: <input></input>
-               Type: <select>
-               <option value="gel">Volvo</option>
-               <option value="conditioner">Saab</option>
-               <option value="cream">Opel</option>
-               <option value="shampoo">Audi</option>
-             </select>
-                <AddButton/>
-            </div>
+            <form>
+                Name: <input type="text" id="name"></input> <br/>
+                Brand: <input type="text" id="brand"></input> <br/>
+                Rating: <input type="text" id="rating"></input> <br/>
+                Type: <select id="type">
+                    <option value="gel">Gel</option>
+                    <option value="conditioner">Conditioner</option>
+                    <option value="cream">Cream</option>
+                    <option value="shampoo">Shampoo</option>
+                </select> <br/>
+                <input type="file" name="fileToUpload" id="image"/> <br/>
+                <button onClick=""> Save </button>
+            </form>
         );
       }
 }
